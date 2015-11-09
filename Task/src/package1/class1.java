077d1f3832bc2a9160b77006a384a7e5c19e05d1
Ahsan <ahsankhan541@gmail.com>
2015-11-09 06:03:18 -0800

    this is second commit

--- Task/src/package1/class1.java
+++ Task/src/package1/class1.java
@@ -1,5 +1,8 @@
 package package1;
 
 public class class1 {
-
-}
+	public static void main(String h[]){
+System.out.println("this is the second commit");
+System.out.println("this is the second commit");
+System.out.println("this is the second commit");
+}}
